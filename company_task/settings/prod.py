d36bd9fdfd004e74ca1a2ee1493476e2e1e6486f
MiKPo4eLiK <stassojcuk@gmail.com>
2025-08-19 18:54:58 +0300

    preparation for rendering

--- company_task/settings/prod.py
+++ company_task/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["it-company-task-manager-onro.onrender.com"]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
